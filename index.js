cde699cd5defed6a32162c77b40fe8518e542555
Jakub Petriska <jakubpetriska@gmail.com>
2018-01-26 22:22:21 +0100

    Tweak the fuck out of this app

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
+const FAVICON_SIZE = 48;
+const FAVICON_BORDER_SIZE = 3;
+
 const SECONDS_PER_HOUR = 60 * 60;
 const SECONDS_PER_MINUTE = 60;
 
-let params = (new URL(document.location)).searchParams;
-let to = params.get("to");
+const formatNumberToTwoDigits = (number) => {
+  const str = number + '';
+  if (str.length == 1) {
+    return '0' + str;
+  } else {
+    return str;
+  }
+};
+
+const params = (new URL(document.location)).searchParams;
+const to = params.get("to");
+const from = params.get("from");
 
 let data = null;
 if (to) {
   data = {
     to: moment(to),
+    from: moment(from),
+    secondsLeftTotal: -1,
     secondsLeft: -1,
     countdownUpdateIntervalId: -1,
+    faviconLink: null,
+    lastFaviconStep: -1
   };
 } else {
   data = {
     menuDatepicker: null,
     menuTimepicker: null,
-    date: moment().format('YYYY-MM-DD'),
-    time: moment().format('HH:mm'),
+    date: moment().add(2, 'h').format('YYYY-MM-DD'),
+    time: moment().add(2, 'h').format('HH:mm'),
   };
 }
 
@@ -26,9 +43,12 @@ const vm = new Vue({
   created: function () {
     if (this.to) {
       this.updateSecondsLeft();
+      this.secondsLeftTotal =this.to.unix() - this.from.unix();
+      this.updateFavicon();
       this.countdownUpdateIntervalId = setInterval(
         () => {
           this.updateSecondsLeft();
+          this.updateFavicon();
           document.title = this.timeLeftString;
           if (this.secondsLeft <= 0) {
             clearInterval(this.countdownUpdateIntervalId);
@@ -42,12 +62,58 @@ const vm = new Vue({
   methods: {
     startCountdown: function () {
       const pickedDatetime = moment(this.date + ' ' + this.time);
-      const countdownTimeParam = 'to=' + pickedDatetime.toISOString();
-      const url = window.location.origin + window.location.pathname + '?' + countdownTimeParam;
+      const params = [
+        'from=' + moment().toISOString(),
+        'to=' + pickedDatetime.toISOString()
+      ].join('&');
+      const url = window.location.origin + window.location.pathname + '?' + params;
       window.location.href = url;
     },
     updateSecondsLeft: function () {
-      this.secondsLeft = this.to.unix() - moment().unix();
+      this.secondsLeft = Math.max(this.to.unix() - moment().unix(), 0);
+    },
+    updateFavicon: function () {
+      const progressPercentage =
+        Math.min(Math.max((this.secondsLeftTotal - this.secondsLeft) / this.secondsLeftTotal, 0), 1);
+      const faviconStep = Math.round(progressPercentage * 50);
+      if (this.lastFaviconStep == faviconStep) {
+        return;
+      }
+      this.lastFaviconStep = faviconStep;
+
+      if (this.faviconLink) {
+        this.faviconLink.remove();
+      }
+
+      const canvas = document.createElement('canvas');
+      canvas.width = FAVICON_SIZE;
+      canvas.height = FAVICON_SIZE;
+      const ctx = canvas.getContext('2d');
+
+      ctx.fillStyle = 'rgb(90, 90, 90)';
+      ctx.fillRect(0, 0, FAVICON_SIZE, FAVICON_SIZE);
+      ctx.fillStyle = '#000000';
+      ctx.fillRect(0, 0, FAVICON_SIZE, FAVICON_BORDER_SIZE);
+      ctx.fillRect(0, 0, FAVICON_BORDER_SIZE, FAVICON_SIZE);
+      ctx.fillRect(FAVICON_SIZE - FAVICON_BORDER_SIZE, 0, FAVICON_BORDER_SIZE, FAVICON_SIZE);
+      ctx.fillRect(0, FAVICON_SIZE - FAVICON_BORDER_SIZE, FAVICON_SIZE, FAVICON_BORDER_SIZE);
+
+      const greenValue = Math.min(Math.round(255 * (1 - progressPercentage) * 2), 255);
+      const redValue = Math.min(Math.round(255 * progressPercentage * 2), 255);
+      ctx.fillStyle = `rgb(${redValue}, ${greenValue}, 0)`;
+      ctx.fillRect(
+        FAVICON_BORDER_SIZE,
+        FAVICON_BORDER_SIZE,
+        Math.round((FAVICON_SIZE - FAVICON_BORDER_SIZE * 2) * progressPercentage),
+        FAVICON_SIZE - FAVICON_BORDER_SIZE * 2
+      );
+
+      const link = document.createElement('link');
+      link.type = 'image/x-icon';
+      link.rel = 'shortcut icon';
+      link.href = canvas.toDataURL("image/x-icon");
+      document.getElementsByTagName('head')[0].appendChild(link);
+      this.faviconLink = link;
     }
   },
   computed: {
@@ -59,7 +125,9 @@ const vm = new Vue({
       const minutesLeft = Math.floor(currentSecondsLeft / SECONDS_PER_MINUTE);
       currentSecondsLeft -= minutesLeft * SECONDS_PER_MINUTE;
 
-      return hoursLeft + ':' + minutesLeft + ':' + currentSecondsLeft;
+      return [hoursLeft, minutesLeft, currentSecondsLeft]
+        .map(formatNumberToTwoDigits)
+        .join(':');
     }
   }
 });
