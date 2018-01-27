2ff89a55e2e25ebe3dd96020083f24792f21d47c
Jakub Petriska <jakubpetriska@gmail.com>
2018-01-27 22:10:52 +0100

    Refactor everything

--- index.js
+++ index.js
@@ -17,48 +17,14 @@ const params = (new URL(document.location)).searchParams;
 const to = params.get("to");
 const from = params.get("from");
 
-let data = null;
-if (to) {
-  data = {
-    to: moment(to),
-    from: moment(from),
-    secondsLeftTotal: -1,
-    secondsLeft: -1,
-    countdownUpdateIntervalId: -1,
-    faviconLink: null,
-    lastFaviconStep: -1
-  };
-} else {
-  data = {
+Vue.component('home', {
+  template: '#home',
+  data: () => ({
     menuDatepicker: null,
     menuTimepicker: null,
     date: moment().add(2, 'h').format('YYYY-MM-DD'),
     time: moment().add(2, 'h').format('HH:mm'),
-  };
-}
-
-const vm = new Vue({
-  el: "#app",
-  data,
-  created: function () {
-    if (this.to) {
-      this.updateSecondsLeft();
-      this.secondsLeftTotal =this.to.unix() - this.from.unix();
-      this.updateFavicon();
-      this.countdownUpdateIntervalId = setInterval(
-        () => {
-          this.updateSecondsLeft();
-          this.updateFavicon();
-          document.title = this.timeLeftString;
-          if (this.secondsLeft <= 0) {
-            clearInterval(this.countdownUpdateIntervalId);
-            document.title = 'Finished!';
-          }
-        },
-        1000
-      );
-    }
-  },
+  }),
   methods: {
     startCountdown: function () {
       const pickedDatetime = moment(this.date + ' ' + this.time);
@@ -68,7 +34,39 @@ const vm = new Vue({
       ].join('&');
       const url = window.location.origin + window.location.pathname + '?' + params;
       window.location.href = url;
-    },
+    }
+  }
+});
+
+Vue.component('countdown', {
+  template: '#countdown',
+  data: () => ({
+    to: moment(to),
+    from: moment(from),
+    secondsLeftTotal: -1,
+    secondsLeft: -1,
+    countdownUpdateIntervalId: -1,
+    faviconLink: null,
+    lastFaviconStep: -1
+  }),
+  created: function () {
+    this.updateSecondsLeft();
+    this.secondsLeftTotal =this.to.unix() - this.from.unix();
+    this.updateFavicon();
+    this.countdownUpdateIntervalId = setInterval(
+      () => {
+        this.updateSecondsLeft();
+        this.updateFavicon();
+        document.title = this.timeLeftString;
+        if (this.secondsLeft <= 0) {
+          clearInterval(this.countdownUpdateIntervalId);
+          document.title = 'Finished!';
+        }
+      },
+      1000
+    );
+  },
+  methods: {
     updateSecondsLeft: function () {
       this.secondsLeft = Math.max(this.to.unix() - moment().unix(), 0);
     },
@@ -129,5 +127,12 @@ const vm = new Vue({
         .map(formatNumberToTwoDigits)
         .join(':');
     }
+  }
+});
+
+const vm = new Vue({
+  el: "#app",
+  data: {
+    to
   }
 });
